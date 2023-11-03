266a4ab691fddb6b797650dc53a48d32df4684f6
Sumit Yadav <syadav1@greencellmobility.com>
2023-11-03 07:20:01 +0530

    installed whitenoise for static files in production

--- Greencell/settings.py
+++ Greencell/settings.py
@@ -14,11 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aj6fjx^gi-9ql_08!u&7odja+dd)fvp495%n0b$%09ns5u+bsl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sumitgcm.azurewebsites.net']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['sumitgcm.azurewebsites.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,7 +52,7 @@ ROOT_URLCONF = 'Greencell.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
