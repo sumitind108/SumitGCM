3e9f63d4fafecd03826c9c26fed84e8e1fe6a21d
Sumit Yadav <syadav1@greencellmobility.com>
2023-11-03 08:05:46 +0530

    added pages

--- HelloGCM/urls.py
+++ HelloGCM/urls.py
@@ -5,8 +5,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('', views.index, name='index'),
     path('plot/', views.process_and_plot, name='process_and_plot'),
+
+    path('plot_template/', views.plot_template, name='plot_template'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- HelloGCM/views.py
+++ HelloGCM/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .plot_generator import generate_plots  # Corrected import statement
+from django.http import HttpResponse, HttpResponseRedirect
+
+
 
 def index(request):
     return render(request, 'HelloGCM/index.html')
@@ -10,3 +13,8 @@ def process_and_plot(request):
 
     # Render the template with the plot paths
     return render(request, 'HelloGCM/plot_template.html', {'plot_paths': plot_paths})
+
+
+def plot_template(request):
+    # Add any logic or data you need here
+    return render(request, 'HelloGCM/plot_template.html')  # Ensure the correct path to your template
